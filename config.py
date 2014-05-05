e5d77207091a7f9c9b0fc7b0ff7fb5eb77ccdf53
pmaddi <pranav.maddi@gmail.com>
2014-05-05 15:04:54 -0400

    trying again

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
