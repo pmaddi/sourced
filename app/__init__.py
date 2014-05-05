be1316f5fe0d3fc3b16b6280ad659bcf0ac45794
pmaddi <pranav.maddi@gmail.com>
2014-05-05 14:41:04 -0400

    trying shit to get it on heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
 from flask.ext.login import LoginManager
 # from flask.ext.openid import OpenID
 from config import basedir
@@ -8,6 +9,8 @@ from config import basedir
 app = Flask(__name__)
 app.config.from_object('config')
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+heroku = Heroku(app)
+
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
